e4faf26f27c08ac3cde0c503d015aaeda6304d23
ironman-collab <uk6118660@gmail.com>
2024-10-21 13:54:31 +0500

    Add files via upload

---
+++ Task1.cpp
@@ -0,0 +1,85 @@
+//Umar Khalid
+//SEction A_2
+//Task-1
+#include<iostream>
+using namespace std;
+int main()
+//{
+//	cout << "************" << endl;
+//	cout << "Hello World" << endl;
+//	cout << "************" << endl;
+//	return 0;
+//}
+// Task-2
+//{
+//	cout << "************" << endl;
+//	cout << "*          *" << endl;
+//	cout << "************" << endl;
+//	return 0;
+//}
+//Task-3
+//{
+//
+//	cout << "    *" << endl;
+//	cout << "   * *" << endl;
+//	cout << "  *   *" << endl;
+//	cout << " *******" << endl;
+//	return 0;
+//}
+//Task-4
+
+	/*{
+
+		cout << "   *" << endl;
+		cout << "  ***" << endl;
+		cout << " *****" << endl;
+		cout << "*******" << endl;
+		return 0;
+	}*/
+//Task-5
+	/*{
+		 cout << " ****" << endl;
+		cout << "*    *" << endl;
+		cout << "*    *" << endl;
+		cout << " **** " << endl;
+		return 0;
+	}*/
+	//TAsk-6
+//{
+//	cout << " *******" << endl;
+//	cout << " *     *" << endl;
+//	cout << " *     *" << endl;
+//	cout << " ******* " << endl;
+//	return 0;
+//}
+
+//Task-7
+//{
+//	cout << "   *" << endl;
+//	cout << " *   *" << endl;
+//	cout << "*     *" << endl;
+//	cout << " *   *" << endl;
+//	cout << "   *" << endl;
+//	return 0;
+//}
+//Task-8
+//{
+//	cout << "    *" << endl;
+//	cout << "  *****" << endl;
+//	cout << "*********" << endl;
+//	cout << "  *****" << endl;
+//	cout << "    *" << endl;
+//	return 0;
+//}
+//Task-9
+{
+	{
+		cout << "@@@@@@@@@@@@@@" << endl;
+		cout << "@  @      @  @" << endl;
+		cout << "@ @        @ @" << endl;
+		cout << "@@          @@" << endl;
+		cout << "@            @" << endl;
+		cout << "@@@@@@@@@@@@@@" << endl;
+		return 0;
+	}
+}
